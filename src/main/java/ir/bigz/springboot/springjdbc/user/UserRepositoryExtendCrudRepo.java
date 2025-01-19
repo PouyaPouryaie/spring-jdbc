dea02fe3cfec9cec16bbb12bf73d2becd27c9fbe
pouya <pouyapouryaie@gmail.com>
2025-01-19 18:18:09 -0400

    use @Query to use spring-data-JDBC

--- src/main/java/ir/bigz/springboot/springjdbc/user/UserRepositoryExtendCrudRepo.java
+++ src/main/java/ir/bigz/springboot/springjdbc/user/UserRepositoryExtendCrudRepo.java
@@ -1,6 +1,14 @@
 package ir.bigz.springboot.springjdbc.user;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepositoryExtendCrudRepo extends CrudRepository<User, Long> {
+
+
+    @Modifying
+    @Query("UPDATE users set name = :name, email = :email, updated_on = NOW(), version = version + 1 where id = :id and version = :version")
+    boolean updateUser(@Param("id") long id, @Param("name") String name, @Param("email") String email, @Param("version") int version);
 }
