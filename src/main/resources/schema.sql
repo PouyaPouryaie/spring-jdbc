85676a40bf61442b58b975728599cfce08f85f84
pouya <pouyapouryaie@gmail.com>
2025-01-19 14:02:07 -0400

    update spring boot version to 3.4.1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,23 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id serial primary key,
-  name varchar(100) NOT NULL,
-  email varchar(100) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL primary key,
+    name varchar(100) NOT NULL,
+    email varchar(100) DEFAULT NULL,
+    created_on TIMESTAMP,
+    updated_on TIMESTAMP
 );
 
 DROP TABLE IF EXISTS Post;
 
 CREATE TABLE Post (
-                      id varchar(255) NOT NULL,
-                      title varchar(255) NOT NULL,
-                      slug varchar(255) NOT NULL,
-                      date date NOT NULL,
-                      time_to_read int NOT NULL,
-                      tags varchar(255),
-                      version INT,
-                      PRIMARY KEY (id)
+    id varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    slug varchar(255) NOT NULL,
+--     content TEXT,        -- Content of the post (can be longer text)
+--     published_on TIMESTAMP WITH TIME ZONE, -- Date and time the post was published
+--     updated_on TIMESTAMP WITH TIME ZONE,  -- Date and time the post was last updated
+    date date NOT NULL,
+    time_to_read int NOT NULL,
+    tags varchar(255),
+    version INT,
+    PRIMARY KEY (id)
 );
